e3eda10e6f21eb102dcc2cc2e9c0d7e48dca620c
phuwarit bunbanlu <youremail@yourdomain.com>
2021-02-05 18:06:21 +0700

    update

---
+++ week5.py
@@ -0,0 +1,43 @@
+'''class nisit :
+    def __init__(self,name,Student_code,sex,year,faculty,department,province) :
+        self.name = name
+        self.Student_code = Student_code
+        self.sex = sex
+        self.year = year
+        self.faculty = faculty
+        self.department = department
+        self.province = province
+
+    def shownisit(self) :
+        print('{0:-<20}{1:-<20}'.format("","เเนะนำตัว"))
+        print("สวัสดีครับ")
+        print("ชื่อ-นามสกุล : ",self.name)
+        print("รหัสนักศึกษา : ",self.Student_code)
+        print("เพศ : ",self.sex)
+        print("ชั้นปีการศึกษา : ",self.year)
+        print("คณะ : ",self.faculty)
+        print("สาขาวิชา : ",self.department)
+        print("ภูมิลำเนา : ",self.province)
+
+a = str(input("ชื่อ-นามสกุล : "))
+b = str(input("รหัสนักศึกษา : ")) 
+c = str(input("เพศ : ")) 
+d = str(input("ชั้นปีการศึกษา : ")) 
+e = str(input("คณะ : ")) 
+f = str(input("สาขาวิชา : ")) 
+g = str(input("ภูมิลำเนา : "))
+
+x = nisit(a,b,c,d,e,f,g)
+x.shownisit()'''
+
+from os import
+class shop :
+    def __init__(self) :
+        
+
+    def Show_products() :
+        
+
+    def add_products() :
+
+
